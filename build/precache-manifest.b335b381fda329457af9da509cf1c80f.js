20af37372b80c4ba54bec8fcce3d0c9a650e1084
milhie <emiliedelsol85@gmail.com>
2020-05-02 21:34:31 +0200

    Ajout de l'application react arbres de vie (diaporamas) et ajout de toutes les images

---
+++ build/precache-manifest.b335b381fda329457af9da509cf1c80f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbef86bdaa1a309129d1e380d5976d00",
+    "url": "http://localhost/Milhie/GIT/milhiecreationPHPintegrationReact/build/index.html"
+  },
+  {
+    "revision": "f1a4bb1c7205247dfb40",
+    "url": "http://localhost/Milhie/GIT/milhiecreationPHPintegrationReact/build/static/css/2.d4afeea5.chunk.css"
+  },
+  {
+    "revision": "208a53fd1749e139347d",
+    "url": "http://localhost/Milhie/GIT/milhiecreationPHPintegrationReact/build/static/css/main.4b2999f1.chunk.css"
+  },
+  {
+    "revision": "f1a4bb1c7205247dfb40",
+    "url": "http://localhost/Milhie/GIT/milhiecreationPHPintegrationReact/build/static/js/2.ce1f2440.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "http://localhost/Milhie/GIT/milhiecreationPHPintegrationReact/build/static/js/2.ce1f2440.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "208a53fd1749e139347d",
+    "url": "http://localhost/Milhie/GIT/milhiecreationPHPintegrationReact/build/static/js/main.5ebc5889.chunk.js"
+  },
+  {
+    "revision": "32c60d922089888514be",
+    "url": "http://localhost/Milhie/GIT/milhiecreationPHPintegrationReact/build/static/js/runtime-main.c60b0a2f.js"
+  }
+]);

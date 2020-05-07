fe6a844741984a6571608070cb02645f56ab6a27
milhie <emiliedelsol85@gmail.com>
2020-05-08 00:22:32 +0200

    test avec un nouveau build (modif des styles)

---
+++ buildS2/precache-manifest.5a6e17c8249c3b773ac30c641fb40415.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b42e931da6f00907e61f02fe2a7d35eb",
+    "url": "https://php.milhie.fr/buildS/index.html"
+  },
+  {
+    "revision": "b5bf3d63ccb4996c0869",
+    "url": "https://php.milhie.fr/buildS/static/css/2.d4afeea5.chunk.css"
+  },
+  {
+    "revision": "b0c2c9b64b3b17dd5e84",
+    "url": "https://php.milhie.fr/buildS/static/css/main.0979c023.chunk.css"
+  },
+  {
+    "revision": "b5bf3d63ccb4996c0869",
+    "url": "https://php.milhie.fr/buildS/static/js/2.f262a341.chunk.js"
+  },
+  {
+    "revision": "b0c2c9b64b3b17dd5e84",
+    "url": "https://php.milhie.fr/buildS/static/js/main.a2d9ee6f.chunk.js"
+  },
+  {
+    "revision": "b463e3698635d72e80a1",
+    "url": "https://php.milhie.fr/buildS/static/js/runtime~main.e3e1b086.js"
+  }
+]);

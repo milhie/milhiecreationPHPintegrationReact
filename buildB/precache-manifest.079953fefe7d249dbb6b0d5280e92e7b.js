8d22c0e7627047fd6380d975275d7fa16fa40a39
milhie <emiliedelsol85@gmail.com>
2020-05-10 23:24:43 +0200

    modification build, ajout des images sécurisées dans buildB

---
+++ buildB/precache-manifest.079953fefe7d249dbb6b0d5280e92e7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb1a090275ce90f61e99b8416d1d21a2",
+    "url": "https://php.milhie.fr/buildB/index.html"
+  },
+  {
+    "revision": "23a5c3237fe9464b9386",
+    "url": "https://php.milhie.fr/buildB/static/css/main.10d0a746.chunk.css"
+  },
+  {
+    "revision": "211ccf99a32c2d97c2a6",
+    "url": "https://php.milhie.fr/buildB/static/js/2.3dba88b1.chunk.js"
+  },
+  {
+    "revision": "5c2e12d960b32c7afa7a21958f5ec469",
+    "url": "https://php.milhie.fr/buildB/static/js/2.3dba88b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23a5c3237fe9464b9386",
+    "url": "https://php.milhie.fr/buildB/static/js/main.b35c828c.chunk.js"
+  },
+  {
+    "revision": "85793cc4dfa55f723327",
+    "url": "https://php.milhie.fr/buildB/static/js/runtime-main.157e5e90.js"
+  }
+]);

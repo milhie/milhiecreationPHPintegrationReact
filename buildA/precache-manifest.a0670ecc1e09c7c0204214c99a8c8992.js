0f7527db5ed65dadbf5d5b39811e8efa7a38973c
milhie <emiliedelsol85@gmail.com>
2020-05-10 21:55:53 +0200

    Ajout d'un nouveau build buildA avec nouveau carousel

---
+++ buildA/precache-manifest.a0670ecc1e09c7c0204214c99a8c8992.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fec9a8dd22224292cfbe4e1fa714918",
+    "url": "https://php.milhie.fr/buildA/index.html"
+  },
+  {
+    "revision": "eff8d17648a5554b38eb",
+    "url": "https://php.milhie.fr/buildA/static/css/main.10d0a746.chunk.css"
+  },
+  {
+    "revision": "4fcb68cf32dc8baa59f2",
+    "url": "https://php.milhie.fr/buildA/static/js/2.bf9bb936.chunk.js"
+  },
+  {
+    "revision": "5c2e12d960b32c7afa7a21958f5ec469",
+    "url": "https://php.milhie.fr/buildA/static/js/2.bf9bb936.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eff8d17648a5554b38eb",
+    "url": "https://php.milhie.fr/buildA/static/js/main.2998335f.chunk.js"
+  },
+  {
+    "revision": "541000f80dd36e363570",
+    "url": "https://php.milhie.fr/buildA/static/js/runtime-main.ba04bdb8.js"
+  }
+]);
---
+++ buildtest/precache-manifest.b93b93f3664b060ea7b3c0cd81c19b2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dca3dda892711f4e89d813ca7a9471fa",
+    "url": "https://php.milhie.fr/buildtest/index.html"
+  },
+  {
+    "revision": "1e861c1d5a2f62f3be9c",
+    "url": "https://php.milhie.fr/buildtest/static/css/main.10d0a746.chunk.css"
+  },
+  {
+    "revision": "8a36c2225cdace6e0958",
+    "url": "https://php.milhie.fr/buildtest/static/js/2.910aa180.chunk.js"
+  },
+  {
+    "revision": "1e861c1d5a2f62f3be9c",
+    "url": "https://php.milhie.fr/buildtest/static/js/main.861658ba.chunk.js"
+  },
+  {
+    "revision": "346a40bdf8a63f2ec168",
+    "url": "https://php.milhie.fr/buildtest/static/js/runtime~main.37f01e6d.js"
+  }
+]);

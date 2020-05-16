546e70f194912406ed5404f2d4eecb49da98928a
milhie <emiliedelsol85@gmail.com>
2020-05-16 13:17:34 +0200

    ajout d'un nouveau build avec images en .webp, modification de stylaAccueil.css pour mettre images en .webp

---
+++ buildWebp/precache-manifest.89e16458ba09cc5e9480161d42d267f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bda4e518c0b90193315bd713c40cf69",
+    "url": "https://php.milhie.fr/buildWebp/index.html"
+  },
+  {
+    "revision": "a6db24039b0fbd9e1d43",
+    "url": "https://php.milhie.fr/buildWebp/static/css/main.5eaaa798.chunk.css"
+  },
+  {
+    "revision": "3892a85cd3005fa23daf",
+    "url": "https://php.milhie.fr/buildWebp/static/js/2.13923851.chunk.js"
+  },
+  {
+    "revision": "5c2e12d960b32c7afa7a21958f5ec469",
+    "url": "https://php.milhie.fr/buildWebp/static/js/2.13923851.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6db24039b0fbd9e1d43",
+    "url": "https://php.milhie.fr/buildWebp/static/js/main.5f685467.chunk.js"
+  },
+  {
+    "revision": "6287e49435904e857c85",
+    "url": "https://php.milhie.fr/buildWebp/static/js/runtime-main.914e2aff.js"
+  }
+]);

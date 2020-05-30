94fcc8c7952353ee6ccdc55663a1ab8ef1e669da
milhie <emiliedelsol85@gmail.com>
2020-05-31 00:18:21 +0200

    ajout d'un build colliers dans le dossier buildBijoux et ajout du script dans colliers.php

---
+++ buildBijoux/buildColliers/precache-manifest.dafe27080ed8726359551b25c710fcf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba5755e032ef3d50b35ec003999543a7",
+    "url": "https://php.milhie.fr/buildBijoux/buildColliers/index.html"
+  },
+  {
+    "revision": "8f6d235b89d14b1077b2",
+    "url": "https://php.milhie.fr/buildBijoux/buildColliers/static/css/main.423531c7.chunk.css"
+  },
+  {
+    "revision": "6c20d983c52e8d0a5236",
+    "url": "https://php.milhie.fr/buildBijoux/buildColliers/static/js/2.300f18f6.chunk.js"
+  },
+  {
+    "revision": "5c2e12d960b32c7afa7a21958f5ec469",
+    "url": "https://php.milhie.fr/buildBijoux/buildColliers/static/js/2.300f18f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f6d235b89d14b1077b2",
+    "url": "https://php.milhie.fr/buildBijoux/buildColliers/static/js/main.ec3a2a95.chunk.js"
+  },
+  {
+    "revision": "d6a0f92dea53ccbec185",
+    "url": "https://php.milhie.fr/buildBijoux/buildColliers/static/js/runtime-main.24977d34.js"
+  }
+]);

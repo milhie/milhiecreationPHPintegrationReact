dc483013b229332bd06df8deda3b255df8cc2773
milhie <emiliedelsol85@gmail.com>
2020-05-31 23:45:11 +0200

    ajout d'une classe contain sur les div en dessous du script decos noel pour qu'il y ait la meme mise en page que pour les carousel

--- autresDecorations.php
+++ autresDecorations.php
@@ -8,7 +8,6 @@
         <link rel="stylesheet" type="text/css" href="side-menu.css">
         <link rel="stylesheet" type="text/css" href="style_pages.css">
         <link rel="stylesheet" type="text/css" href="stylebase.css">
-        <link rel="stylesheet" type="text/css" href="diapo.css">
         <link rel="shortcut icon" href="images/favicon.ico">
         <link href="https://fonts.googleapis.com/css2?family=Calligraffitti&family=Dancing+Script:wght@400;500;600;700&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Alex+Brush&display=swap" rel="stylesheet">
@@ -44,7 +43,6 @@
                         <h2 class="epingle"><img src="images/ico_epingle.png" alt="Accueil" class="ico_categorie" />Autres décorations</h2>
                     </header>
 
-                    <nav class="navigation">
                         <div>
                             <header class="description">
                                 <h2>Décorations de Noël</h2>
@@ -53,24 +51,25 @@
 
                             <noscript>You need to enable JavaScript to run this app.</noscript><div id="root"></div><script>!function(f){function e(e){for(var r,t,n=e[0],o=e[1],u=e[2],i=0,l=[];i<n.length;i++)t=n[i],a[t]&&l.push(a[t][0]),a[t]=0;for(r in o)Object.prototype.hasOwnProperty.call(o,r)&&(f[r]=o[r]);for(s&&s(e);l.length;)l.shift()();return c.push.apply(c,u||[]),p()}function p(){for(var e,r=0;r<c.length;r++){for(var t=c[r],n=!0,o=1;o<t.length;o++){var u=t[o];0!==a[u]&&(n=!1)}n&&(c.splice(r--,1),e=i(i.s=t[0]))}return e}var t={},a={1:0},c=[];function i(e){if(t[e])return t[e].exports;var r=t[e]={i:e,l:!1,exports:{}};return f[e].call(r.exports,r,r.exports,i),r.l=!0,r.exports}i.m=f,i.c=t,i.d=function(e,r,t){i.o(e,r)||Object.defineProperty(e,r,{enumerable:!0,get:t})},i.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},i.t=function(r,e){if(1&e&&(r=i(r)),8&e)return r;if(4&e&&"object"==typeof r&&r&&r.__esModule)return r;var t=Object.create(null);if(i.r(t),Object.defineProperty(t,"default",{enumerable:!0,value:r}),2&e&&"string"!=typeof r)for(var n in r)i.d(t,n,function(e){return r[e]}.bind(null,n));return t},i.n=function(e){var r=e&&e.__esModule?function(){return e.default}:function(){return e};return i.d(r,"a",r),r},i.o=function(e,r){return Object.prototype.hasOwnProperty.call(e,r)},i.p="https://php.milhie.fr/buildDecos/buildDecosNoel/";var r=window.webpackJsonp=window.webpackJsonp||[],n=r.push.bind(r);r.push=e,r=r.slice();for(var o=0;o<r.length;o++)e(r[o]);var s=n;p()}([])</script><script src="https://php.milhie.fr/buildDecos/buildDecosNoel/static/js/2.ee1196fb.chunk.js"></script><script src="https://php.milhie.fr/buildDecos/buildDecosNoel/static/js/main.475fba3e.chunk.js"></script>
                         </div>
-                        <div class="Diapos">
+
+                        <div class="contain">
                             <header class="description">
                                 <h2>Portes bijoux</h2>
                                 <p>Fait main en fil d'aluminium et perles</p>
                             </header>
 
-                            <ul class="diapo" id="porteBjx">
+                            <ul id="porteBjx">
                                 <li class="item">Porte bijoux à suspendres 30cm de diamètre: 30€ pièce...<img src="images/autresDecos/arbreBjx1.jpeg" alt="arbre porte bijoux"></li>
                                 <li class="item"></li>
                             </ul>
                         </div>
-                        <div class="Diapos">
+                        <div class="contain">
                             <header class="description">
                                 <h2>Décorations de fenêtre</h2>
                                 <p>Fait main en fil d'aluminium et perles</p>
                             </header>
 
-                            <ul class="diapo" id="Windows">
+                            <ul id="Windows">
                                 <li class="item"><p>Piège à rêve porte plante aériennes pour décoration de fenêtre ou décoration murale. 30€ vendu sans plantes.<img src="images/autresDecos/decoWin1.jpeg" alt="piège à rêve porte plante"></p></li>
                                 <li class="item"><p>Piège à rêve porte plante aériennes pour décoration de fenêtre ou décoration murale. 30€ vendu sans plantes.<img src="images/autresDecos/decoWin2.jpeg" alt="piège à rêve porte plante"></p></li>
                                 <li class="item"></li>

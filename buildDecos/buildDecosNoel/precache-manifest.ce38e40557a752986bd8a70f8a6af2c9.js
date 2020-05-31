fb96b1258570d2b2b68589f3c627d43366444c67
milhie <emiliedelsol85@gmail.com>
2020-05-31 23:03:48 +0200

    ajout du nouveau build bijoux et integration du script dans autresDecos.php

---
+++ buildDecos/buildDecosNoel/precache-manifest.ce38e40557a752986bd8a70f8a6af2c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43dc112ca2339f237fe753054e98a307",
+    "url": "https://php.milhie.fr/buildDecos/buildDecosNoel/index.html"
+  },
+  {
+    "revision": "9ea284b3dadbe45f2e19",
+    "url": "https://php.milhie.fr/buildDecos/buildDecosNoel/static/css/main.06db0548.chunk.css"
+  },
+  {
+    "revision": "64b9bf57ffeeb50f811a",
+    "url": "https://php.milhie.fr/buildDecos/buildDecosNoel/static/js/2.ee1196fb.chunk.js"
+  },
+  {
+    "revision": "9ea284b3dadbe45f2e19",
+    "url": "https://php.milhie.fr/buildDecos/buildDecosNoel/static/js/main.e0d9efab.chunk.js"
+  },
+  {
+    "revision": "52b38aa9779f4b96a597",
+    "url": "https://php.milhie.fr/buildDecos/buildDecosNoel/static/js/runtime~main.3590671b.js"
+  }
+]);

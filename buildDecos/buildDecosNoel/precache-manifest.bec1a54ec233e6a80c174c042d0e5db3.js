69c0e54f3ac5bac3ea3bed86ebc0cc14d41ef7b3
milhie <emiliedelsol85@gmail.com>
2020-05-31 22:53:17 +0200

    ajout du build decos et ajout du script dans autresdecorations.php

---
+++ buildDecos/buildDecosNoel/precache-manifest.bec1a54ec233e6a80c174c042d0e5db3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2dae97f5ae30c888507bb6fc9e5f26c",
+    "url": "https://php.milhie.fr/buildDecos/buildDecosNoel/index.html"
+  },
+  {
+    "revision": "6f45afffc7bb192fa518",
+    "url": "https://php.milhie.fr/buildDecos/buildDecosNoel/static/css/main.4d8f45ca.chunk.css"
+  },
+  {
+    "revision": "64b9bf57ffeeb50f811a",
+    "url": "https://php.milhie.fr/buildDecos/buildDecosNoel/static/js/2.ee1196fb.chunk.js"
+  },
+  {
+    "revision": "6f45afffc7bb192fa518",
+    "url": "https://php.milhie.fr/buildDecos/buildDecosNoel/static/js/main.8ae2c83c.chunk.js"
+  },
+  {
+    "revision": "52b38aa9779f4b96a597",
+    "url": "https://php.milhie.fr/buildDecos/buildDecosNoel/static/js/runtime~main.3590671b.js"
+  }
+]);

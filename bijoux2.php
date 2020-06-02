90681b613ec2a76c17a1c1ca74d88a4aac63e0b6
milhie <emiliedelsol85@gmail.com>
2020-06-03 01:03:44 +0200

    creation d'un nouveau fichier bijoux2.php et ajout du build avec l'application avec tous les bijoux

---
+++ bijoux2.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        
+        <meta charset="utf-8" />
+        <meta name="description" content="site milhie creation, presentation de bijoux, arbres de vie, decorations en fil d'aluminium, perles, pierres de litotherapie. Tout est fait main par moi-même.">    
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" type="text/css" href="side-menu.css">
+        <link rel="stylesheet" type="text/css" href="stylebase.css">
+        <link rel="shortcut icon" href="images/favicon.ico">
+        <link href="https://fonts.googleapis.com/css2?family=Calligraffitti&family=Dancing+Script:wght@400;500;600;700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Alex+Brush&display=swap" rel="stylesheet">
+        <title>Milhie Création bijoux</title>
+    </head>
+
+    <body>
+
+        <section id="page">
+            <div id="element">
+                <header id="title">
+                    <?php include("tete_de_page.php"); ?>
+                </header>
+
+                <nav>
+                    <?php include("menu.php"); ?>
+                </nav>
+            </div>
+            
+            
+            
+            <section id="main">
+                <h2 class="titre">Créations en perle et fil d'aluminium, bijoux, décorations, pièges à rêves, arbres de vie , lytothérapie. </h2>
+                <article class="content">
+                    <h2 class="titre">Créations en perles et fils d'aluminium</h2>
+                
+
+                    
+                    <p class="return"><a href="bijoux.php">RETOUR</a></p>
+
+
+                </article>
+            </section>
+            <footer>
+                <?php include("pied_de_page.php"); ?>
+                <?php include("compteur/compteurBijoux.php"); ?>
+            </footer>
+        </section>
+    <script src="ui.js"></script>
+
+    </body>
+</html>
---
+++ buildBijoux/buildBjxComplet/precache-manifest.9b88ff34c83ac793c7ff01c9b0bdb84a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8f02a6e85138c654e4bc7bb0633517d",
+    "url": "https://php.milhie.fr/buildBijoux/buildBjxComplet/index.html"
+  },
+  {
+    "revision": "2aa97dca9d160726ecd3",
+    "url": "https://php.milhie.fr/buildBijoux/buildBjxComplet/static/css/main.50935391.chunk.css"
+  },
+  {
+    "revision": "0918df392f10b88c6ae0",
+    "url": "https://php.milhie.fr/buildBijoux/buildBjxComplet/static/js/2.512cf25b.chunk.js"
+  },
+  {
+    "revision": "5c2e12d960b32c7afa7a21958f5ec469",
+    "url": "https://php.milhie.fr/buildBijoux/buildBjxComplet/static/js/2.512cf25b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aa97dca9d160726ecd3",
+    "url": "https://php.milhie.fr/buildBijoux/buildBjxComplet/static/js/main.d401495d.chunk.js"
+  },
+  {
+    "revision": "c52eb0cd5c8dd9d1bbf5",
+    "url": "https://php.milhie.fr/buildBijoux/buildBjxComplet/static/js/runtime-main.8cb1a0ee.js"
+  }
+]);

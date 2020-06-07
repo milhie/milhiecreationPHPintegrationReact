472c3b783b5777747f0bfa4f8773466f1a03e1cc
milhie <emiliedelsol85@gmail.com>
2020-06-07 11:20:11 +0200

    ajout du nouveau buildBijxComplet et modification de bijoux2.php pour y integrer ce nouveau build.

---
+++ buildBijoux/buildBjxComplet/precache-manifest.aa55fe31603fb8b49fd48aebb68a3d03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de45f2e9bff6c2a5cdecad9187f1373d",
+    "url": "https://php.milhie.fr/buildBijoux/buildBjxComplet/index.html"
+  },
+  {
+    "revision": "6cc8fd08388265eac85b",
+    "url": "https://php.milhie.fr/buildBijoux/buildBjxComplet/static/css/main.166469f1.chunk.css"
+  },
+  {
+    "revision": "0918df392f10b88c6ae0",
+    "url": "https://php.milhie.fr/buildBijoux/buildBjxComplet/static/js/2.512cf25b.chunk.js"
+  },
+  {
+    "revision": "5c2e12d960b32c7afa7a21958f5ec469",
+    "url": "https://php.milhie.fr/buildBijoux/buildBjxComplet/static/js/2.512cf25b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cc8fd08388265eac85b",
+    "url": "https://php.milhie.fr/buildBijoux/buildBjxComplet/static/js/main.0c3b5dc6.chunk.js"
+  },
+  {
+    "revision": "c52eb0cd5c8dd9d1bbf5",
+    "url": "https://php.milhie.fr/buildBijoux/buildBjxComplet/static/js/runtime-main.8cb1a0ee.js"
+  }
+]);

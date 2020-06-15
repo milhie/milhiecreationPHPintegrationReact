c8744e6b67885821d4652fced539bd3da5e933c9
milhie <emiliedelsol85@gmail.com>
2020-06-16 00:07:21 +0200

    copie de decos_mariage.php en decos_mariage_copie.php, intégration de la nouvelle application décos de table de mariage dans decos_mariage.php

---
+++ buildDecos/buildDecosMariage/precache-manifest.4ab69c9dbd674a7150ac8db3ffee7c06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2fa109445679b54a1912597e05274cf",
+    "url": "https://php.milhie.fr/buildDecos/buildDecosMariage/index.html"
+  },
+  {
+    "revision": "db6bf994789f74f54832",
+    "url": "https://php.milhie.fr/buildDecos/buildDecosMariage/static/css/main.ac4b6b93.chunk.css"
+  },
+  {
+    "revision": "8dc43c3bc884342ec90a",
+    "url": "https://php.milhie.fr/buildDecos/buildDecosMariage/static/js/2.bc4c5746.chunk.js"
+  },
+  {
+    "revision": "5c2e12d960b32c7afa7a21958f5ec469",
+    "url": "https://php.milhie.fr/buildDecos/buildDecosMariage/static/js/2.bc4c5746.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db6bf994789f74f54832",
+    "url": "https://php.milhie.fr/buildDecos/buildDecosMariage/static/js/main.31751c63.chunk.js"
+  },
+  {
+    "revision": "01bea0fb22ad41d56d6b",
+    "url": "https://php.milhie.fr/buildDecos/buildDecosMariage/static/js/runtime-main.7dbbf279.js"
+  }
+]);
---
+++ decos_mariage_copie.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        
+        <meta charset="utf-8" />
+        <meta name="description" content="site milhie creation, presentation de bijoux, arbres de vie, decorations en fil d'aluminium, perles, pierres de litotherapie. Tout est fait main par moi-même.">    
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" type="text/css" href="side-menu.css">
+        <link rel="stylesheet" type="text/css" href="style_pages.css">
+        <link rel="stylesheet" type="text/css" href="stylebase.css">
+        <link rel="shortcut icon" href="images/favicon.ico">
+        <link href="https://fonts.googleapis.com/css2?family=Calligraffitti&family=Dancing+Script:wght@400;500;600;700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Alex+Brush&display=swap" rel="stylesheet">        
+        <title>MilhieCréation décorations mariage</title>
+  <script src='js/jquery.min.js'></script>
+
+    </head>
+
+    <body>
+
+    	<section id="page">
+            <div id="element">
+                <header id="title">
+
+                    <?php include("tete_de_page.php"); ?>
+
+
+                </header>   
+
+                <nav>
+                    <?php include("menu.php"); ?>                    
+
+                </nav>
+            </div>
+            
+            <section id="main"> 
+                <h2 class="titre">Créations en perle et fil d'aluminium, bijoux, décorations, pièges à rêves, arbres de vie , lytothérapie. </h2>
+                <article class="contentitem">
+
+                    <!--<h2 class="titre">Créations en perle et fil d'aluminium, bijoux, décorations, pièges à rêves, arbres de vie , lytothérapie. </h2>-->
+
+                    <header>
+                        <h2 class="epingle"><img src="images/ico_epingle.png" alt="Accueil" class="ico_categorie" /> Décorations de mariage.</h2>
+                    </header>
+
+                    <nav class="navigation">
+                        <div class="Diapos">
+                            <header class="description">
+                                <h2>Marques places.</h2>
+                                <p>Idéal pour une table de mariage. Forme et couleur du fil au choix.
+                                    <br/>Prévoir un délais de fabrication et un délais supplémentaire si la couleur demandée n'est pas en stock.
+                                    <br/>Réduction sur les grosses commandes. N'hésitez pas à <a href="mailto:milhiecreation.com"> me contacter</a> 
+                                    <br/> Création de modèles sur mesure.</p>
+                            </header>
+
+                            <ul class="diapo" id="decoMariage">
+                                <li class="item">
+                                    <?php include("decosMariage/diapo1.php"); ?>
+                                </li>
+                                <li class="item">
+                                    <?php include("decosMariage/diapo2.php"); ?>
+                                </li>
+                                <li class="item"><p>Marque place sur verre</p><img src="images/decosMariage/mariage3.jpeg"></li>
+                                <li class="item"><img src="images/decosMariage/mariage4.jpeg"></li>
+                            </ul>
+                        </div>
+                    </nav>
+                </article>
+            </section>
+
+            <footer>
+                <?php include("pied_de_page.php"); ?> 
+                <?php include("compteur/compteurDecosMariage.php"); ?> 
+
+            </footer>
+        </section>
+    <script src="ui.js"></script>
+</body>
+</html>
